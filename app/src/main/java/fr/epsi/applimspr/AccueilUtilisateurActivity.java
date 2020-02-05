c47f4864dce093bb35dea41205084d2bbdae39d5
EL <abdelilah.elouardi@epsi.fr>
2020-02-05 13:03:12 +0100

    script camera

--- app/src/main/java/fr/epsi/applimspr/AccueilUtilisateurActivity.java
+++ app/src/main/java/fr/epsi/applimspr/AccueilUtilisateurActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,13 @@ public class AccueilUtilisateurActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueilutilisateur_activity);
+        Button btnQrCode = findViewById(R.id.buttonQrCode);
+        btnQrCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lancerCamera();
+            }
+        });
         btnListPromo = findViewById(R.id.buttonListPromo);
         btnListPromo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +30,15 @@ public class AccueilUtilisateurActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
+
+    public void lancerCamera(){
+        Intent intent2 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //startActivity(intent2);
+        int REQUEST_ID_IMAGE_CAPTURE = 100;
+        //Start camera and wait for the results.
+        this.startActivityForResult(intent2, REQUEST_ID_IMAGE_CAPTURE);
+    }
+
+
 }
