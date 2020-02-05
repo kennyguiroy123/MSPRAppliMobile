dc45a729903318b32c36757796f6a3dff56287f8
Guiroy Kenny <kenny.guiroy@epsi.fr>
2020-02-05 11:08:04 +0100

    ButtonClickNewLayout

---
+++ app/src/main/java/fr/epsi/applimspr/AccueilUtilisateurActivity.java
@@ -0,0 +1,13 @@
+package fr.epsi.applimspr;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccueilUtilisateurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accueilutilisateur_activity);
+    }
+}
--- app/src/main/java/fr/epsi/applimspr/MainActivity.java
+++ app/src/main/java/fr/epsi/applimspr/MainActivity.java
@@ -2,13 +2,35 @@ package fr.epsi.applimspr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnConnexion;
+
+    public static void display(AccueilUtilisateurActivity activity){
+        Intent intent=new Intent(activity,AccueilUtilisateurActivity.class);
+        activity.startActivityForResult(intent,0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.header_appli);
+        setContentView(R.layout.main_activity);
+        btnConnexion = findViewById(R.id.buttonConnexion);
+        btnConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.display(AccueilUtilisateurActivity.this);
+            }
+        });
+    }
+
+    public void openActivityUtilisateur(){
+        Intent accueilUtilisateur = new Intent(this, AccueilUtilisateurActivity.class);
+        startActivity(accueilUtilisateur);
     }
 }
