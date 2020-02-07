44c4e7d49b0f741a1387c20578ceb9ea987363d8
Emixham <Maximed.ipod@gmail.com>
2020-02-07 12:28:34 +0100

    WIP¨AJOUT WS2

--- app/src/main/java/fr/epsi/applimspr/MainActivity.java
+++ app/src/main/java/fr/epsi/applimspr/MainActivity.java
@@ -1,41 +1,19 @@
 package fr.epsi.applimspr;
 
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Point;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
-
 import fr.epsi.applimspr.model.Utilisateur;
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button btnConnexion;
@@ -49,12 +27,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-
-
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-
-
         btnConnexion = findViewById(R.id.buttonConnexion);
         login = findViewById(R.id.editTextUsername);
         password = findViewById(R.id.editTextPassword);
@@ -71,14 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 } catch (NoSuchAlgorithmException e){
                     e.printStackTrace();
                 }
-                JsonArray param = new JsonArray();
-                JsonObject username = new JsonObject();
-                username.addProperty("username",login.toString());
-                param.add(username);
-                JsonObject p = new JsonObject();
-                p.addProperty("password",pwd);
-                param.add(p);
-                new WebService(urlStr, new WebService.HttpAsyTaskListener() {
+                new WebService(urlStr, login.toString(), pwd, new WebService.HttpAsyTaskListener() {
                     @Override
                     public void webServiceDone(String result) {
                         initData(result);
@@ -100,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         //JSONObject jsonObject;
         //jsonObject=new JSONObject(data);
         //JSONArray jsonArray=jsonObject.getJSONArray("items");
-        Utilisateur user=new Utilisateur(login.toString(),password.toString(),token);
+        //Utilisateur user=new Utilisateur(login.toString(),password.toString(),token);
 
 
 
--- app/src/main/java/fr/epsi/applimspr/WebService.java
+++ app/src/main/java/fr/epsi/applimspr/WebService.java
@@ -2,6 +2,10 @@ package fr.epsi.applimspr;
 
 import android.os.AsyncTask;
 
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,6 +14,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -22,15 +27,23 @@ public class WebService extends AsyncTask<Void,Void,Object>{
 
         private HttpAsyTaskListener httpAsyTaskListener;
         private String urlStr;
+        private String password;
+        private String login;
 
-    public WebService(String url,HttpAsyTaskListener listener){
+    public WebService(String url,String login,String password,HttpAsyTaskListener listener){
         httpAsyTaskListener=listener;
         urlStr=url;
+        this.password = password;
+        this.login = login;
+/*        String urlParameters  = "Fname=Ram&Lname=Ramesh&Msisdn=6785459898";
+        byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+        int postDataLength = postData.length;*/
+        String request = "http://10.0.2.2:8000/test/";
     }
 
         @Override
         protected Object doInBackground(Void... voids) {
-        return call(urlStr);
+        return call(urlStr,login,password);
     }
 
         @Override
@@ -44,17 +57,26 @@ public class WebService extends AsyncTask<Void,Void,Object>{
     }
 
 
-        public Object call(String urlStr) {
+        public Object call(String urlStr,String login,String pwd) {
         try {
             java.net.URL url = null;
             url = new URL(urlStr);
-            HttpURLConnection urlConnection;
-                urlConnection = (HttpURLConnection) url.openConnection();
+            HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+            conn.setDoOutput( true );
+            conn.setInstanceFollowRedirects( false );
+            conn.setRequestMethod( "POST" );
+            conn.setRequestProperty( "Content-Type", "application/json");
+            conn.setRequestProperty("login",login);
+            conn.setRequestProperty("password",pwd);
+            conn.setRequestProperty( "charset", "utf-8");
+            conn.setUseCaches(false);
+            //conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
             try {
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                conn.connect();
+                InputStream in = new BufferedInputStream(conn.getInputStream());
                 return convertStreamToString(in);
             } finally {
-                urlConnection.disconnect();
+                conn.disconnect();
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
